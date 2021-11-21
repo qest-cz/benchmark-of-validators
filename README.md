c0f818f6f5592ac32ac5ece6edbca11eabb05a18
Cyril Urban <cyril.urban@ack.ee>
2021-11-21 13:02:35 +0100

    ref(script): reduce count of libraries in the script, update results in the readme

--- README.md
+++ README.md
@@ -19,25 +19,18 @@ Platform info:
    Memory: 15 GB
 
 Suite: Simple object
-✔ io-ts                2,820,844 rps
-✔ zod                    854,230 rps
-✔ validator.js           720,204 rps
-✔ validate.js            366,178 rps
-✔ validatorjs            213,912 rps
-✔ joi                    184,973 rps
-✔ ajv                  9,193,894 rps
-✔ mschema                932,706 rps
-✔ parambulator            34,459 rps
-✔ yup                     43,402 rps
+✔ joi                   193,703 rps
+✔ yup                    50,833 rps
+✔ ajv                 9,103,966 rps
+✔ validate.js           265,246 rps
+✔ io-ts               1,973,238 rps
+✔ zod                   820,109 rps
 
-   io-ts              -69.32%      (2,820,844 rps)   (avg: 354ns)
-   zod                -90.71%        (854,230 rps)   (avg: 1μs)
-   validator.js       -92.17%        (720,204 rps)   (avg: 1μs)
-   validate.js        -96.02%        (366,178 rps)   (avg: 2μs)
-   validatorjs        -97.67%        (213,912 rps)   (avg: 4μs)
-   joi                -97.99%        (184,973 rps)   (avg: 5μs)
-   ajv                     0%      (9,193,894 rps)   (avg: 108ns)
-   mschema            -89.86%        (932,706 rps)   (avg: 1μs)
-   parambulator       -99.63%         (34,459 rps)   (avg: 29μs)
-   yup                -99.53%         (43,402 rps)   (avg: 23μs)
+   joi               -97.87%        (193,703 rps)   (avg: 5μs)
+   yup               -99.44%         (50,833 rps)   (avg: 19μs)
+   ajv                    0%      (9,103,966 rps)   (avg: 109ns)
+   validate.js       -97.09%        (265,246 rps)   (avg: 3μs)
+   io-ts             -78.33%      (1,973,238 rps)   (avg: 506ns)
+   zod               -90.99%        (820,109 rps)   (avg: 1μs)
+-----------------------------------------------------------------------
 ```
--- src/index.ts
+++ src/index.ts
@@ -4,247 +4,178 @@ import * as fpFunction from 'fp-ts/function'
 import * as t from 'io-ts'
 import * as p from 'io-ts/lib/PathReporter'
 import * as zod from 'zod'
-import * as ValidatorJs from 'validator.js'
-import * as Validator from 'validatorjs'
 import * as validate from 'validate.js'
 import * as Joi from 'joi'
 import Ajv from 'ajv'
 import ajvFormat from 'ajv-formats'
-import * as mschema from 'mschema'
-import * as parambulator from 'parambulator'
 import * as yup from 'yup'
 
-const is = ValidatorJs.Assert
-const validator = ValidatorJs.validator()
 const ajv = new Ajv()
 ajvFormat(ajv)
 
 const benchmark = new Benchmarkify('Validators benchmark').printHeader()
 const bench = benchmark.createSuite('Simple object')
 
-const obj = {
-    name: 'Cyril Urban',
-    email: 'cyril.urban@qest.net',
-    firstName: 'Qest',
-    phone: '731123456',
-    age: 25,
+const person = {
+    name: 'Cyril Urban', // required string, min 4 chars, max 25 chars
+    email: 'cyril.urban@random.com', // required string, email
+    company: 'Qest', // optional string
+    phone: '731123456', // required string
+    age: 25, // required number, min 18
 }
 
-// ---- io-ts ----
-;(() => {
-    const validateModel = (raw, type) => {
-        const onLeft = () => {
-            throw new Error(p.PathReporter.report(type.decode(raw)).toString())
+    // ---- joi ----
+    ; (() => {
+        const schema = Joi.object().keys({
+            name: Joi.string().min(4).max(25).required(),
+            email: Joi.string().email().required(),
+            company: Joi.string().optional(),
+            phone: Joi.string().required(),
+            age: Joi.number().integer().min(18).required(),
+        })
+
+        bench.add('joi', () => {
+            return schema.validate(person)
+        })
+    })()
+
+    // ---- yup ----
+    ; (() => {
+        const schema = yup.object().shape({
+            name: yup.string().min(4).max(25).required(),
+            email: yup.string().email().required(),
+            company: yup.string().optional(),
+            phone: yup.string().required(),
+            age: yup.number().integer().min(18).required(),
+        })
+
+        bench.add('yup', () => {
+            return schema.isValid(person)
+        })
+    })()
+
+    // ---- ajv ----
+    ; (() => {
+        const schema = {
+            properties: {
+                name: { type: 'string', minLength: 4, maxLength: 25 },
+                email: { type: 'string', format: 'email' },
+                company: { type: 'string' },
+                phone: { type: 'string' },
+                age: { type: 'integer', minimum: 18 },
+            },
+            required: ['name', 'email', 'phone', 'age'],
         }
 
-        const res = fpFunction.pipe(
-            type.decode(raw),
-            fpEither.fold(onLeft, function (toReturn) {
-                return toReturn
-            }),
-        )
-        return res
-    }
+        const validate = ajv.compile(schema)
 
-    const validateEmail = (email) => {
-        const re =
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        return re.test(email)
-    }
-
-    interface NameBrand {
-        readonly Name: unique symbol
-    }
-    interface EmailBrand {
-        readonly Email: unique symbol
-    }
-    interface AgeBrand {
-        readonly Age: unique symbol
-    }
-
-    const schema = t.type({
-        name: t.brand(t.string, (s): s is t.Branded<string, NameBrand> => s.length >= 4 && s.length <= 25, 'Name'),
-        email: t.brand(t.string, (s): s is t.Branded<string, EmailBrand> => validateEmail(s), 'Email'),
-        firstName: t.unknown,
-        phone: t.unknown,
-        age: t.brand(t.number, (n): n is t.Branded<number, AgeBrand> => n >= 18, 'Age'),
-    })
-
-    bench.add('io-ts', () => {
-        return validateModel(obj, schema)
-    })
-})()
-
-// ---- zod ----
-;(() => {
-    const schema = zod.object({
-        name: zod.string().min(4).max(25),
-        email: zod.string().email(),
-        firstName: zod.any(),
-        phone: zod.any(),
-        age: zod.number().int().min(18),
-    })
-
-    bench.add('zod', () => {
-        return schema.parse(obj)
-    })
-})()
-
-// ---- validator.js ----
-;(() => {
-    const constraints = {
-        name: [is.notBlank(), is.ofLength({ min: 4, max: 25 })],
-        email: is.email(),
-        firstName: is.notBlank(),
-        phone: is.notBlank(),
-        age: [is.required(), is.greaterThan(18)],
-    }
-
-    bench.add('validator.js', () => {
-        validator.validate(obj, constraints)
-    })
-})()
-
-// ---- validate.js ----
-;(() => {
-    const constraints = {
-        name: {
-            presence: true,
-            length: {
-                minimum: 4,
-                maximum: 25,
-            },
-        },
-        email: { email: true },
-        firstName: { presence: true },
-        phone: { presence: true },
-        age: {
-            numericality: {
-                onlyInteger: true,
-                greaterThan: 18,
-            },
-        },
-    }
-
-    bench.add('validate.js', () => {
-        return validate(obj, constraints)
-    })
-})()
-
-// ---- validatorjs ----
-;(() => {
-    const constraints = {
-        name: 'required|min:4|max:25',
-        email: 'required|email',
-        firstName: 'required',
-        phone: 'required',
-        age: 'required|integer|min:18',
-    }
-
-    bench.add('validatorjs', () => {
-        const validation = new Validator(obj, constraints)
-        return validation.passes()
-    })
-})()
-
-// ---- joi ----
-;(() => {
-    const constraints = Joi.object().keys({
-        name: Joi.string().min(4).max(25).required(),
-        email: Joi.string().email().required(),
-        firstName: Joi.required(),
-        phone: Joi.required(),
-        age: Joi.number().integer().min(18).required(),
-    })
-
-    bench.add('joi', () => {
-        return constraints.validate(obj)
-    })
-})()
-
-// ---- ajv ----
-;(() => {
-    const constraints = {
-        properties: {
-            name: { type: 'string', minLength: 4, maxLength: 25 },
-            email: { type: 'string', format: 'email' },
-            firstName: { type: 'string' },
-            phone: { type: 'string' },
-            age: { type: 'integer', minimum: 18 },
-        },
-        required: ['name', 'email', 'firstName', 'phone', 'age'],
-    }
-
-    const validate = ajv.compile(constraints)
-
-    bench.add('ajv', () => {
-        return validate(obj)
-    })
-})()
-
-// ---- mschema ----
-;(() => {
-    const constraints = {
-        name: {
-            type: 'string',
-            minLength: 4,
-            maxLength: 25,
-        },
-        email: 'string',
-        firstName: 'string',
-        phone: 'string',
-        age: {
-            type: 'number',
-            min: 18,
-        },
-    }
-
-    bench.add('mschema', () => {
-        return mschema.validate(obj, constraints)
-    })
-})()
-
-// ---- parambulator ----
-;(() => {
-    const constraints = {
-        name: {
-            type$: 'string',
-            required$: true,
-            minlen$: 4,
-            maxlen$: 25,
-        },
-        email: { type$: 'string', required$: true },
-        firstName: { type$: 'string', required$: true },
-        phone: { type$: 'string', required$: true },
-        age: {
-            type$: 'number',
-            required$: true,
-            min$: 18,
-        },
-    }
-
-    const check = parambulator(constraints)
-
-    bench.add('parambulator', () => {
-        return check.validate(obj, (err) => {
-            //console.log(err);
+        bench.add('ajv', () => {
+            return validate(person)
         })
-    })
-})()
+    })()
 
-// ---- yup ----
-;(() => {
-    const schema = yup.object().shape({
-        name: yup.string().min(4).max(25).required(),
-        email: yup.string().email().required(),
-        firstName: yup.mixed().required(),
-        phone: yup.mixed().required(),
-        age: yup.number().integer().min(18).required(),
-    })
+    // ---- validate.js ----
+    ; (() => {
+        const schema = {
+            name: {
+                type: 'string',
+                presence: true,
+                length: {
+                    minimum: 4,
+                    maximum: 25,
+                },
+            },
+            email: { type: 'string', presence: true, email: true },
+            company: { type: 'string', presence: false },
+            phone: { type: 'string', presence: true },
+            age: {
+                numericality: {
+                    onlyInteger: true,
+                    greaterThan: 18,
+                },
+                presence: true,
+            },
+        }
 
-    bench.add('yup', () => {
-        return schema.isValid(obj)
-    })
-})()
+        bench.add('validate.js', () => {
+            return validate(person, schema)
+        })
+    })()
+
+    // ---- io-ts ----
+    ; (() => {
+        const validateModel = <I, O>(
+            raw: I,
+            type: t.TypeC<any> | t.PartialC<any> | t.ArrayC<any> | t.IntersectionC<any>,
+        ): O => {
+            const onLeft = () => {
+                throw new Error(p.PathReporter.report(type.decode(raw)).toString())
+            }
+
+            return fpFunction.pipe(
+                type.decode(raw),
+                fpEither.fold(onLeft, function (toReturn) {
+                    return toReturn
+                }),
+            )
+        }
+
+        const validateEmail = (email: string) => {
+            const re =
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return re.test(email)
+        }
+
+        interface NameBrand {
+            readonly Name: unique symbol
+        }
+        interface EmailBrand {
+            readonly Email: unique symbol
+        }
+        interface AgeBrand {
+            readonly Age: unique symbol
+        }
+
+        const required = t.type({
+            name: t.brand(t.string, (s): s is t.Branded<string, NameBrand> => s.length >= 4 && s.length <= 25, 'Name'),
+            email: t.brand(t.string, (s): s is t.Branded<string, EmailBrand> => validateEmail(s), 'Email'),
+            phone: t.string,
+            age: t.brand(t.number, (n): n is t.Branded<number, AgeBrand> => n >= 18, 'Age'),
+        })
+
+        const optional = t.partial({
+            company: t.string,
+        })
+
+        const schema = t.intersection([
+            required,
+            optional,
+        ])
+
+        bench.add('io-ts', () => {
+            return validateModel(person, schema)
+        })
+
+        // usage of type
+        // type Person = t.TypeOf<typeof schema>
+    })()
+
+    // ---- zod ----
+    ; (() => {
+        const schema = zod.object({
+            name: zod.string().min(4).max(25),
+            email: zod.string().email(),
+            company: zod.string().optional(),
+            phone: zod.string(),
+            age: zod.number().int().min(18),
+        })
+
+        bench.add('zod', () => {
+            return schema.parse(person)
+        })
+
+        // usage of type
+        // type Person = zod.TypeOf<typeof schema>
+    })()
 
 bench.run()
